1632d65aa18308a19eaa7b34123a11163143f720
HugoLuo <1120242819@bit.edu.cn>
2025-09-24 21:04:04 +0800

    complete the lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple1() {
+        IntList lst = IntList.of(3, 0, 16, 17, 2);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 0 -> 16 -> 289 -> 4", lst.toString());
+        assertTrue(changed);
+    }
 }
